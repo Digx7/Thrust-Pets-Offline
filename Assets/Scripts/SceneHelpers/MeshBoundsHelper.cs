0d0a6ea2fcc9604356df7b3bee57708b25f8a153
Digx7 <37136301+Digx7@users.noreply.github.com>
2026-05-14 12:37:34 -0700

    Fixing bug with frustum culling
    Fixed a bug where objects would stop rendering sooner than expected.
    This was due to the vertex shader not playing nice with the camera's frustum culling.
    We've since added a script called MeshBoundsHelper that needs to be attached to any given object with a vertex shader

---
+++ Assets/Scripts/SceneHelpers/MeshBoundsHelper.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class MeshBoundsHelper : MonoBehaviour 
+{
+    public float boundsSizeMultiplier = 100000f;
+    public bool runsOnStart = true;
+    public bool runsOnUpdate = false;
+    
+    private Mesh _mesh;
+
+    void Awake()
+    {
+        _mesh = GetComponent<MeshFilter>().mesh;
+    }
+
+    void Start() 
+    {
+        if(runsOnStart)
+        {
+            UpdateBounds();
+        }
+    }
+
+    void Update()
+    {
+        if(runsOnUpdate)
+        {
+            UpdateBounds();
+        }
+    }
+
+    void UpdateBounds()
+    {
+        Bounds bounds = mesh.bounds;
+        bounds.size *= boundsSizeMultiplier;
+
+
+        _mesh.bounds = bounds;
+    }
+}
