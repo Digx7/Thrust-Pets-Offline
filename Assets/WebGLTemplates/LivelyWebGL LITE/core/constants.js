bf7b286e05bf4f69182e3c804e44259cc8b1ceab
Digx7 <37136301+Digx7@users.noreply.github.com>
2026-07-12 01:21:19 -0700

    Build 0.2.18.1

--- Assets/WebGLTemplates/LivelyWebGL LITE/core/constants.js
+++ Assets/WebGLTemplates/LivelyWebGL LITE/core/constants.js
@@ -11,14 +11,14 @@ window.ConstantsLib = {
     // Display Setup
     ////////////////////////////////////////////////////
     displayScalingMode: 1,
-    UseDynamicAspectRatio: false,
+    UseDynamicAspectRatio: true,
     aspectX: 16,          // user wants to set aspect ratio
     aspectY: 9,          // user wants to set aspect ratio
     defaultWidth: 1920,
     defaultHeight: 1080,
-    PageSetup_MaxDPI: 2,
-    PageSetup_DefaultDPR: 2,
-    fullscreenHidesCornerButton: true,
+    PageSetup_MaxDPI: 1,
+    PageSetup_DefaultDPR: 0.5,
+    fullscreenHidesCornerButton: false,
 
     ////////////////////////////////////////////////////
     // Loading Screen
@@ -39,7 +39,7 @@ window.ConstantsLib = {
     ////////////////////////////////////////////////////
     // Hamburger / Corner Button
     ////////////////////////////////////////////////////
-    hideCornerButton: true,                     // hide fullscreen/menu button
+    hideCornerButton: false,                     // hide fullscreen/menu button
 
     ////////////////////////////////////////////////////
     // Page Setup
